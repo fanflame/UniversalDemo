668416fff5e77cc927ea0f2a02b406d4022344e5
fanqiang <fan.qiang_2499@immomo.com>
2019-04-08 12:42:51 +0800

    添加自定义graldeplugin以及使用javassit操作class

--- Demo_Gradleplugin/src/main/groovy/com/fanyiran/gradle_plugindemo/MyClassTransform.groovy
+++ Demo_Gradleplugin/src/main/groovy/com/fanyiran/gradle_plugindemo/MyClassTransform.groovy
@@ -1,9 +1,16 @@
 package com.fanyiran.gradle_plugindemo
+
+import com.android.build.api.transform.DirectoryInput
+import com.android.build.api.transform.Format
+import com.android.build.api.transform.JarInput
 import com.android.build.api.transform.QualifiedContent
 import com.android.build.api.transform.Transform
 import com.android.build.api.transform.TransformException
+import com.android.build.api.transform.TransformInput
 import com.android.build.api.transform.TransformInvocation
 import com.android.build.gradle.internal.pipeline.TransformManager
+import com.android.utils.FileUtils
+import org.apache.commons.codec.digest.DigestUtils
 import org.gradle.api.Project
 
 public class MyClassTransform extends Transform {
@@ -14,27 +21,57 @@ public class MyClassTransform extends Transform {
     }
 
     @Override
-    String getName() {
+    public String getName() {
         return "MyClassTransform"
     }
 
     @Override
-    Set<QualifiedContent.ContentType> getInputTypes() {
+    public Set<QualifiedContent.ContentType> getInputTypes() {
         return TransformManager.CONTENT_CLASS
     }
 
     @Override
-    Set<? super QualifiedContent.Scope> getScopes() {
+    public Set<? super QualifiedContent.Scope> getScopes() {
         return TransformManager.SCOPE_FULL_PROJECT
     }
 
     @Override
-    boolean isIncremental() {
+    public boolean isIncremental() {
         return false
     }
 
     @Override
-    void transform(TransformInvocation transformInvocation) throws TransformException, InterruptedException, IOException {
+    public void transform(TransformInvocation transformInvocation) throws TransformException, InterruptedException, IOException {
+        System.out.println("----------------进入transform了--------------")
 
+        //遍历input
+        transformInvocation.inputs.each { TransformInput input ->
+            //遍历文件夹
+            input.directoryInputs.each { DirectoryInput directoryInput ->
+                //注入代码
+                MyInjects.inject(directoryInput.file.absolutePath, project)
+
+                // 获取output目录
+                def dest = transformInvocation.outputProvider.getContentLocation(directoryInput.name,
+                        directoryInput.contentTypes, directoryInput.scopes, Format.DIRECTORY)//这里写代码片
+
+                // 将input的目录复制到output指定目录
+                FileUtils.copyDirectory(directoryInput.file, dest)
+            }
+
+            ////遍历jar文件 对jar不操作，但是要输出到out路径
+            input.jarInputs.each { JarInput jarInput ->
+                // 重命名输出文件（同目录copyFile会冲突）
+                def jarName = jarInput.name
+                println("jar = " + jarInput.file.getAbsolutePath())
+                def md5Name = DigestUtils.md5Hex(jarInput.file.getAbsolutePath())
+                if (jarName.endsWith(".jar")) {
+                    jarName = jarName.substring(0, jarName.length() - 4)
+                }
+                def dest = transformInvocation.outputProvider.getContentLocation(jarName + md5Name, jarInput.contentTypes, jarInput.scopes, Format.JAR)
+                FileUtils.copyFile(jarInput.file, dest)
+            }
+        }
+        System.out.println("--------------结束transform了----------------")
     }
 }
--- app/src/main/java/com/fanyiran/universaldemo/MainActivity.java
+++ app/src/main/java/com/fanyiran/universaldemo/MainActivity.java
@@ -2,6 +2,9 @@ package com.fanyiran.universaldemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.fanyiran.gradle_plugindemo.MyPlguinTestClass;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((TextView) findViewById(R.id.tv)).setText(MyPlguinTestClass.str);
     }
 }
